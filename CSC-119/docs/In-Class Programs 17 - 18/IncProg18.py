3855e41e5532d6d7e97c2a3db65db08a277cf36c
m-gaucher <m-gaucher@users.noreply.github.com>
2017-11-09 07:10:06 -0700

    Create IncProg18.py

---
+++ CSC-119/docs/In-Class Programs 17 - 18/IncProg18.py
@@ -0,0 +1,10 @@
+#open file in write mode 
+output_file = open("data_out.txt", "w") 
+
+#write some line to the file 
+output_file.write("First line to the file") 
+output_file.write("Second line to the file") 
+output_file.write("Third line to the file") 
+
+#close file 
+output_file.close()

d25e38100d20c3faeb99c13e54314b9f9e7e637d
m-gaucher <m-gaucher@users.noreply.github.com>
2017-12-07 14:56:25 -0700

    Update Jagged_Matrix.py

--- CSC-119/docs/Program 1/Jagged_Matrix.py
+++ CSC-119/docs/Program 1/Jagged_Matrix.py
@@ -1,11 +1,11 @@
 
 # declare 2d jagged list (5x5)
 jagged_matrix = [[0, 1, 2, 3, 4 ], 
-	             [5, 6, 7],
-	             [8, 9,10,11], 
-	             [12], 
-	             [13,14],
-	             [15,16,17,18,19,20]]
+	         [5, 6, 7],
+	         [8, 9,10,11], 
+	         [12], 
+	         [13,14],
+	         [15,16,17,18,19,20]]
 
 print("non_sq_matrix dimensions")            
 print("num of rows:", len(jagged_matrix))
